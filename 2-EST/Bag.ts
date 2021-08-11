f9f4c597b4c05049932febb93101373bad556daa
Jorge Londoño <jmlon@yahoo.com>
2021-08-11 11:15:57 -0500

    Updates

---
+++ 2-EST/Bag.ts
@@ -0,0 +1,59 @@
+/**
+ * Bag: Implementación de la bolsa en typescript
+ * 
+ * @author: Jorge Londoño
+ */
+
+import assert from "assert";
+
+
+class Bag<T> implements Iterable<T> {
+
+    private items: Array<T> = [];
+    private n: number = 0;
+
+    add(item: T): void { 
+        this.items.push(item);
+        this.n++;
+    }
+
+    isEmpty(): boolean {
+        return this.n==0;
+    }
+
+    size(): number {
+        return this.n;
+    }
+
+    [Symbol.iterator]() {
+        let pos = 0;
+        const items = this.items;
+
+        return {
+            next(): IteratorResult<T> {
+                if (pos<items.length) {
+                    return { value: items[pos++], done: false };
+                }
+                return { value: null, done: true };
+            }
+        }
+    }
+
+}
+
+
+const bolsaStr: Bag<string> = new Bag();
+assert.ok(bolsaStr.isEmpty(), "La bolsa debe estar vacia al comienzo");
+assert.strictEqual(bolsaStr.size(), 0, "Tamaño inicial debe ser cero");
+bolsaStr.add("Hola");
+bolsaStr.add("Mundo");
+assert.equal(bolsaStr.size(), 2, "Tamaño incorrecto");
+
+const bolsaNum: Bag<number> = new Bag();
+bolsaNum.add(1);
+bolsaNum.add(2);
+bolsaNum.add(3);
+
+for(let x of bolsaNum) {
+    console.log(x);
+}
