ce18599d5b3fd9479b84d73f72cf8f999d5c0513
Jorge Londoño <jorge.londono@gmail.com>
2026-02-16 09:30:55 -0500

    Updates

---
+++ notas/update.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cp /home/jmlon/GIT/AWS/NotesEA/Est_Alg/01_ADTs/chapter_main.pdf         main_adts.pdf
+cp /home/jmlon/GIT/AWS/NotesEA/Est_Alg/02_EST/chapter_main.pdf          main_EstructurasBasicas.pdf
+cp /home/jmlon/GIT/AWS/NotesEA/Est_Alg/03_ANA/chapter_main.pdf          main_Analisis.pdf
+cp /home/jmlon/GIT/AWS/NotesEA/Est_Alg/04_UF/chapter_main.pdf           main_UF.pdf
+cp /home/jmlon/GIT/AWS/NotesEA/Est_Alg/05_SORT/chapter_main.pdf         main_sorting.pdf
+cp /home/jmlon/GIT/AWS/NotesEA/Est_Alg/06_PQ/chapter_main.pdf           main_priorityQueues.pdf
+cp /home/jmlon/GIT/AWS/NotesEA/Est_Alg/07_SEARCH/chapter_main.pdf       main_search.pdf
+cp /home/jmlon/GIT/AWS/NotesEA/Est_Alg/08_BST/chapter_main.pdf          main_bst.pdf
+cp /home/jmlon/GIT/AWS/NotesEA/Est_Alg/09_HASHTABLE/chapter_main.pdf    main_hashTables.pdf
+cp /home/jmlon/GIT/AWS/NotesEA/Est_Alg/10_GRAPHS/chapter_main.pdf       main_graphs.pdf
+
+
+
